bfa7310111b6036410aa434807b3e8d09f598140
Michael Shepanski <mjs7231@gmail.com>
2022-03-29 15:47:26 -0400

    Update to use OpenWeather Fix external IP Remove plex and sickrage panels

--- pkmeter/pkmeter.py
+++ pkmeter/pkmeter.py
@@ -2,25 +2,41 @@
 import json, os, socket
 import argparse, requests
 import shlex, subprocess
-from datetime import datetime, timedelta
+from datetime import datetime
 from os.path import join
 from jinja2 import Template
 from shutil import copyfile
-from plexapi.server import PlexServer
 
+config = None  # cached config object
 ROOT = os.path.expanduser('~/.pkmeter')
 CACHE = os.path.expanduser('~/.cache/pkmeter')
 BYTE, KB, MB = 1, 1024, 1048576
 BYTES1024 = ((2**50,'P'), (2**40,'T'), (2**30,'G'), (2**20,'M'), (2**10,'K'), (1,'B'))
-DARKSKY_URL = 'https://api.darksky.net/forecast/{apikey}/{coords}'
-EXTERNALIP_URL = 'https://api6.ipify.org/?format=json'
-SICKRAGE_URL = '{host}/api/{apikey}/?cmd=future&limit=10'
-NVIDIA_CMD = '/usr/bin/nvidia-settings'
-NVIDIA_ATTRS = ('nvidiadriverversion', 'gpucoretemp', 'gpucurrentfanspeedrpm',
-    'gpuutilization', 'totaldedicatedgpumemory', 'useddedicatedgpumemory')
-NVIDIA_QUERY = '%s --query=%s' % (NVIDIA_CMD, ' --query='.join(NVIDIA_ATTRS))
 REQUEST_TIMEOUT = 10
-config = None  # cached config object
+
+EXTERNALIP_URL = 'https://api.ipify.org/?format=json'
+NVIDIA_CMD = '/usr/bin/nvidia-settings'
+NVIDIA_ATTRS = [
+    'nvidiadriverversion',
+    'gpucoretemp',
+    'gpucurrentfanspeedrpm',
+    'gpuutilization',
+    'totaldedicatedgpumemory',
+    'useddedicatedgpumemory',
+]
+NVIDIA_QUERY = '%s --query=%s' % (NVIDIA_CMD, ' --query='.join(NVIDIA_ATTRS))
+OPENWEATHER_URL = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={apikey}&exclude=minutely,hourly&units=imperial'  # noqa
+OPENWEATHER_ICONS = {
+    '01': 'clear-{dn}',             # clear sky
+    '02': 'partly-cloudy-{dn}',     # few clouds
+    '03': 'cloudy',                 # scattered clouds
+    '04': 'cloudy',                 # broken clouds
+    '09': 'rain',                   # shower rain
+    '10': 'rain',                   # rain (partly)
+    '11': 'rain',                   # thunderstorm
+    '13': 'snow',                   # snow
+    '50': 'fog',                    # mist
+}
 
 
 class Bunch(dict):
@@ -42,23 +58,8 @@ def _get_config():
     return config
 
 
-def _datetime_to_str(dt):
-    if (datetime.now() - dt) < timedelta(hours=23):
-        return dt.strftime('%-I:%M%p').lower().replace('m','')
-    if (datetime.now() - dt) < timedelta(days=6):
-        return dt.strftime('%a')
-    return dt.strftime('%b %-d')
-
-
 def _celsius_to_fahrenheit(value):
     return int(value * 9 / 5 + 32)
-
-
-def _ignored(title, ignored):
-    for ignore in ignored:
-        if ignore.lower() in title.lower():
-            return True
-    return False
 
 
 def _percent(numerator, denominator, precision=2, maxval=999.9, default=0.0):
@@ -80,22 +81,6 @@ def _rget(obj, attrstr, default=None, delim='.'):
         return default
 
 
-def _safe_unlink(path):
-    try:
-        os.unlink(path)
-    except Exception:
-        pass
-
-
-def _sickrage_datestr(stype, show):
-    if stype == 'missed': return 'Missed'
-    if stype == 'later': return datetime.strptime(show['airdate'], '%Y-%m-%d').strftime('%b %d')
-    airday = show['airs'].split(' ', 1)[0][:3]
-    airtime = show['airs'].split(' ', 1)[1].replace(':00','')
-    airtime = airtime.replace(' AM','a').replace(' PM','p')
-    return 'Today %s' % airtime if stype == 'today' else '%s %s' % (airday, airtime)
-
-
 def _to_int(value, default=None):
     try:
         return int(value)
@@ -111,24 +96,6 @@ def _value_to_str(value, unit=BYTE, precision=0, separator=''):
             conversion = round(value / div, int(precision)) if precision else int(value / div)
             return '%s%s%s' % (conversion, separator, unit)
     return '0%s%s' % (separator, unit)
-
-
-def _video_length(length):
-    hours = int(length / 3600000)
-    minutes = int((length - (hours * 3600000)) / 60000)
-    return '%s:%02d' % (hours, minutes)
-
-
-def _video_title(video):
-    if video.type == 'season':
-        episode = video.episodes()[-1]
-        title = f'{episode.grandparentTitle} {episode.seasonEpisode}'
-    elif video.type == 'episode':
-        title = f'{video.grandparentTitle} {video.seasonEpisode}'
-    else:
-        title = f'{video.title} {video.year}'
-    title = title.replace('s0','s').replace('e0','e')
-    return title[:20]
 
 
 def get_conkyrc(key):
@@ -150,25 +117,33 @@ def get_conkyrc(key):
         handle.write(template.render(**config))
 
 
-def get_darksky(key):
-    """ Fetch weather from DarkSky and copy weather images to cache. """
+def get_openweather(key):
+    """ Fetch weather from OpenWeather and copy weather images to cache. """
     config = _get_config()
-    url = DARKSKY_URL.replace('{apikey}', config.darksky_apikey)
-    url = url.replace('{coords}', config.darksky_coords)
+    coords = config.openweather_coords.split(',')
+    url = OPENWEATHER_URL.replace('{apikey}', config.openweather_apikey)
+    url = url.replace('{lat}', coords[0]).replace('{lon}', coords[1])
     response = requests.get(url, timeout=REQUEST_TIMEOUT)
     with open(join(CACHE, f'{key}.json'), 'w') as handle:
         handle.write(response.content.decode())
         handle.write('\n')
     # copy icons into place
     data = json.loads(response.content.decode())
-    source = join(ROOT, 'img', _rget(data, 'currently.icon')) + '.png'
-    dest = join(CACHE, 'current.png')
-    copyfile(source, dest)
+    iconcode = _rget(data, 'current.weather.0.icon')
+    copy_openweather_icon(iconcode, 'current')
     # copy daily icons into place
-    for i, day in enumerate(_rget(data, 'daily.data')[:4]):
-        source = join(ROOT, 'img', _rget(day, 'icon')) + '.png'
-        dest = join(CACHE, f'day{i}.png')
-        copyfile(source, dest)
+    for i, day in enumerate(_rget(data, 'daily')[:4]):
+        iconcode = _rget(day, 'weather.0.icon')
+        copy_openweather_icon(iconcode, f'day{i}')
+
+
+def copy_openweather_icon(code, day='current'):
+    """ Return the icon filepath from its code 03d, 02n etc.. """
+    code = code or '01d'  # TODO: Make default icon a questionmark
+    num,dn = code[:2], 'day' if code[2] == 'd' else 'night'
+    source = join(ROOT, 'img', OPENWEATHER_ICONS[num].replace('{dn}',dn) + '.png')
+    dest = join(CACHE, f'{day}.png')
+    copyfile(source, dest)
 
 
 def get_externalip(key):
@@ -209,101 +184,13 @@ def get_nvidia(key):
     cmd = shlex.split(f'{NVIDIA_CMD} --glxinfo')
     for line in subprocess.check_output(cmd).decode('utf8').split('\n'):
         if line.strip().lower().startswith('opengl renderer string:'):
-            values['cardname'] = line.split(':', 1)[1].split('/')[0].strip()
+            cardname = line.split(':', 1)[1].split('/')[0]
+            cardname = cardname.replace('NVIDIA', '')
+            cardname = cardname.replace(' Ti', 'ti')
+            values['cardname'] = cardname.strip()
     # save values to cache
     with open(join(CACHE, f'{key}.json'), 'w') as handle:
         json.dump(values, handle)
-
-
-def get_plexadded(key):
-    """ Fetch plex recently added. """
-    values = []
-    plex = PlexServer()
-    config = _get_config()
-    recent = plex.library.recentlyAdded()
-    recent = sorted(recent, key=lambda v:v.addedAt, reverse=True)
-    for vdata in recent[:10]:
-        video = {}
-        video['type'] = vdata.type
-        video['added'] = _datetime_to_str(vdata.addedAt)
-        video['title'] = _video_title(vdata)
-        if not _ignored(video['title'], config.plexadded_ignore):
-            values.append(video)
-    with open(join(CACHE, f'{key}.json'), 'w') as handle:
-        json.dump(values, handle)
-
-
-def get_plexhistory(key):
-    """ Fetch plex recently added. """
-    values = {'items':[]}
-    plex = PlexServer()
-    config = _get_config()
-    accounts = {a.accountID:a.name for a in plex.systemAccounts()}
-    mindate = datetime.now() - timedelta(days=30)
-    history = plex.history(150, mindate=mindate)
-    for vdata in history:
-        video = {}
-        video['type'] = vdata.type
-        video['viewed'] = _datetime_to_str(vdata.viewedAt)
-        video['title'] = _video_title(vdata)[:15]
-        video['account'] = accounts.get(vdata.accountID, 'Unknown')
-        video['account'] = config.plexhistory_names.get(video['account'], video['account'])[:6]
-        if not _ignored(video['account'], config.plexhistory_ignore):
-            values['items'].append(video)
-    values['total'] = len(values['items'])
-    with open(join(CACHE, f'{key}.json'), 'w') as handle:
-        json.dump(values, handle)
-
-
-def get_plexsessions(key):
-    """ Fetch Plex recently added, recently played and now playing. """
-    values = []
-    plex = PlexServer()
-    for i, vdata in enumerate(plex.sessions()):
-        video = {}
-        video['user'] = vdata.usernames[0]
-        video['type'] = vdata.type
-        video['year'] = vdata.year
-        video['duration'] = _video_length(vdata.duration)
-        video['viewoffset'] = _video_length(vdata.viewOffset)
-        video['percent'] = round((vdata.viewOffset / vdata.duration) * 100)
-        video['player'] = vdata.players[0].device if vdata.players else 'NA'
-        video['state'] = vdata.players[0].state if vdata.players else 'NA'
-        video['playstate'] = f'{video["viewoffset"]} of {video["duration"]} ({video["percent"]}%)'
-        video['title'] = _video_title(vdata)
-        if video['state'] == 'playing':
-            values.append(video)
-        # Download the thumbnail to cache
-        url = vdata.show().thumbUrl if vdata.type == 'episode' else vdata.thumbUrl
-        response = requests.get(url, timeout=REQUEST_TIMEOUT)
-        with open(join(CACHE, f'session{i}.jpg'), 'wb') as handle:
-            handle.write(response.content)
-    if len(values) < 1: _safe_unlink(join(CACHE, 'session0.jpg'))
-    if len(values) < 2: _safe_unlink(join(CACHE, 'session1.jpg'))
-    # save values to cache
-    with open(join(CACHE, f'{key}.json'), 'w') as handle:
-        json.dump(values, handle)
-
-
-def get_sickrage(key):
-    """ Fetch upcoming episodes from Sickrage. """
-    values = []
-    config = _get_config()
-    url = SICKRAGE_URL.replace('{host}', config.sickrage_host)
-    url = url.replace('{apikey}', config.sickrage_apikey)
-    response = requests.get(url, timeout=REQUEST_TIMEOUT)
-    data = json.loads(response.content.decode('utf8'))
-    for stype in ('missed','today','soon','later'):
-        for show in _rget(data, f'data.{stype}', []):
-            show['datestr'] = _sickrage_datestr(stype, show)
-            show['show_name'] = show['show_name'][:20]
-            show['episode'] = f's{show.get("season","")}e{show.get("episode","")}'
-            if ((not values or (show['show_name'] != values[-1]['show_name']))
-              and not _ignored(show['show_name'], config.sickrage_ignore)):
-                values.append(show)
-    # save values to cache
-    with open(join(CACHE, f'{key}.json'), 'w') as handle:
-        json.dump(values[:10], handle)
 
 
 def lookup_value(key, opts):
