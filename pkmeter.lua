eeefcff6db6d7f17db74e58248de4057f87d73be
Michael Shepanski <mshepanski@nasuni.com>
2026-04-06 19:00:57 -0400

    Add debug print statement for mouse events in conky_mouse_event function

--- pkmeter.lua
+++ pkmeter.lua
@@ -27,8 +27,8 @@ end
 -- Conky Mouse Event
 -- Called on conky mouse events
 function conky_mouse_event(event)
-  -- print("Mouse event: ".. event.type .." x=".. event.x .." y=".. event.y)
   if event.type == 'button_down' then
+    print("Mouse event: ".. event.type .." x=".. event.x .." y=".. event.y)
     -- Ignore events too close to the window edge
     local margin = 20
     if event.x < margin or event.x > (conky_window.width - margin) then return end
